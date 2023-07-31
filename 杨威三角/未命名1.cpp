37bf179475a0dc30b439945233c7976fc30a2f91
kulat55 <1985954492@qq.com>
2023-07-31 10:54:14 +0800

    涉及到一些简单的算法，和飞机打靶的飞机模型设计有些类似

---
+++ 杨威三角/未命名1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int arr[10][10] = { 0 };
+	for(int i=0;i<10;i++){
+		for(int j=0;j<10;j++){
+			if(j==0)
+			{
+				arr[i][j]=1;
+			}//TODO
+			if(i == j){
+				arr[i][j] = 1;//TODO
+			}
+			if(i >= 2 && j >= 1){
+				arr[i][j] = arr[i - 1][j] + arr [i - 1][j - 1];//TODO
+			}
+		}
+	}
+	//¥Ú”°
+	for(int i=0;i<10;i++){
+			for(int j=0;j<=i;j++){
+				printf("%d ",arr[i][j]);
+			}
+			printf("\n");
+		}
+		return 0;
+}
