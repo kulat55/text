27f3f37010406eb9594906dbabee026c0c32ab71
kulat55 <1985954492@qq.com>
2023-08-25 23:01:57 +0800

    简单的逆序

---
+++ 单词逆序/未命名1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>// µ¥´ÊÄæÐò   l am a boy!  boy! a am l
+#include <stdlib.h>
+#include <string.h>
+void solution(char arr[3][50]){
+ 
+	 char temp;
+	temp = *(char*)arr;
+	*(char*)arr = *((char*)arr + 2);
+	*((char*)arr + 2) = temp;
+	fputs(arr,stdout);
+
+}
+
+int main() {
+    char arr[3][50];
+    
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%s", arr[i][50]);
+    }
+	solution(arr);
+    return 0;
+}
